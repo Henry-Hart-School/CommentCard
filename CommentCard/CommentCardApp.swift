ce09d3974f84c3c5902dfbb573338b47301bba6d
Henry-Hart-School <hart.h@etoncollege.org.uk>
2023-02-07 10:15:03 +0000

    Initial Commit

---
+++ CommentCard/CommentCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCardApp.swift
+//  CommentCard
+//
+//  Created by Hart, Henry (AGDF) on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CommentCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
